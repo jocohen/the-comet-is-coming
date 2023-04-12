747dfa68a5e535041bdb8f201c8de8c436a96bfe
jocohen <jocohen@student.42.fr>
2023-04-12 11:17:22 +0200

    Change main project directory to config name

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the-comet-is-coming.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
